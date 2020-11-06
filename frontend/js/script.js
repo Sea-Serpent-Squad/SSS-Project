613da64d81b7456310aab99e64780cdbe25dcdc2
SgAkErRu <sulmpx60@yandex.ru>
2020-11-07 00:40:17 +0400

    Небольшая доработка интерфейса назначения техники, начало слияния 2 и 3 страницы
    При нажатии на виртуальную технику снизу появляется блок со списком настоящей. После того, как была выбрана настоящая техника, название виртуальной техники меняется на номер настоящей, добавляется операция "дорога" и строка с водителем под этой техникой.

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,7 +3,7 @@
 // и соответственно выделение снималось, когда нажали на другую вирт. технику (или на ту же самую еще один раз)
 function prepareCollapse()
 {
-    document.querySelectorAll(".vehicle").forEach((elem) => {
+    document.querySelectorAll(".virt-vehicle").forEach((elem) => {
         // при клике скрываем текущий открытый блок div collapse
         $(elem).on('click', () => {
             $('.collapse.show').collapse('hide');
@@ -11,15 +11,32 @@ function prepareCollapse()
         // выделяем кружком и блокируем нажатия на ссылки пока не раскроется блок с настоящ. техникой
         $(elem.getAttribute("href")).on('show.bs.collapse', () => {
             elem.children[0].classList.add("selected");
-            document.querySelectorAll(".vehicle").forEach((elem) => { elem.classList.add("no-link") });
+            document.querySelectorAll(".virt-vehicle").forEach((elem) => { elem.classList.add("no-link") });
         });
         // как только раскрылся, включаем ссылки обратно
         $(elem.getAttribute("href")).on('shown.bs.collapse', () => {
-            document.querySelectorAll(".vehicle").forEach((elem) => { elem.classList.remove("no-link") });
+            document.querySelectorAll(".virt-vehicle").forEach((elem) => { elem.classList.remove("no-link") });
         });
         // убираем кружок при закрытии блока с техникой
         $(elem.getAttribute("href")).on('hide.bs.collapse', () => {
             elem.children[0].classList.remove("selected");
+        });
+    });
+}
+
+function vehicleChoice()
+{
+    let arr = Array.from(document.querySelector("#list-of-free-vehicles-1").children);
+    arr.forEach((elem) => {
+        elem.children[0].addEventListener('click', () => {
+            let veh = document.querySelector('a[href="#vehicle1"]');
+            veh.children[1].innerText = elem.children[0].innerText;
+            veh.parentElement.children[2].insertAdjacentHTML('afterbegin',"Дорога<br>");
+            veh.classList.add("text-primary");
+            let driver_row = veh.parentElement.nextElementSibling;
+            driver_row.classList.remove("disabled"); 
+            /* На этом месте мы добавляем в таймлайн дорогу */
+            veh.click();
         });
     });
 }
@@ -35,7 +52,7 @@ function addNewVehicle()
         $('.collapse.show').collapse('hide');
         // формируем новую строку в списке вирт. техники (копируя первую виртуальную технику)
         let list = document.getElementById("virt-list-of-vehicles");
-        let newRow = document.createElement("dl");
+        let newRow = document.createElement("div");
         newRow.className = "row mt-5";
         newRow.innerHTML = list.children[1].innerHTML;
         // теперь меняем значения
@@ -73,18 +90,28 @@ function op_mp() {
     window.open("main_page.html", "_self")
 }
 
-function op_tp() {
-    window.open("third_page.html", "_self")
+function op_save() {
+    window.open("main_page.html", "_self")
 }
 
 
+function nextMsg() {
+    if (messages.length == 0) {
+        op_save();
+    } else {
+        $('#add_save').html(messages.pop()).delay(1000).fadeOut(1000, nextMsg);
+    }
+};
 
+var messages = [
+    "Сохранено!",
+].reverse();
 
 // как только документ прогрузится вызвать эти функции
 $(document).ready(() => {
     document.getElementById('to_mp').addEventListener('click',()=>op_mp());
-    document.getElementById('add_save').addEventListener('click',()=>op_tp());
+    document.getElementById('add_save').addEventListener('click',()=>nextMsg());
     prepareCollapse();
     addNewVehicle();
-
+    vehicleChoice();
 });
